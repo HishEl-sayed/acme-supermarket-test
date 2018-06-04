d0627921a95ba786f7bb7a286da72c6b977b800e
HishEl-sayed <hishamelsayed91@gmail.com>
2018-06-04 03:19:31 +0100

    cleaning indentation

--- src/basket.js
+++ src/basket.js
@@ -1,51 +1,53 @@
 import pricingRules from './rules.js'
-import { FR1, SR1, CF1 } from './items'
+import {
+  FR1,
+  SR1,
+  CF1
+} from './items'
 
 class Basket {
   constructor(rule, ...products) {
-		this.rule = Object.keys(rule)
+    this.rule = Object.keys(rule)
     this.products = []
     this.discountedProductPrices = []
     this.freeItems = 0
     this.bulkItems = 0
   }
-	get Total() {
+  get Total() {
     this.products.forEach((product) => {
       if (product.code === this.rule[0]) {
         return this.discountedProductPrices.push(this.getFree(product.price))
-      }
-      else if (product.code === this.rule[1]) {
+      } else if (product.code === this.rule[1]) {
         return this.discountedProductPrices.push(this.getBulk(product.price))
       }
       return this.discountedProductPrices.push(this.getNoRule(product.price))
     })
     return this.addTotal(this.discountedProductPrices)
   }
-  
+
   add(item) {
-   this.products.push(item)
+    this.products.push(item)
   }
 
-	getFree(freePrice) {
+  getFree(freePrice) {
     this.freeItems++
-    if (this.freeItems % 2 === 0) {
-      return freePrice - freePrice
-    }
+      if (this.freeItems % 2 === 0) {
+        return freePrice - freePrice
+      }
     return freePrice
-	}
+  }
 
-	getBulk(bulkPrice) {
+  getBulk(bulkPrice) {
     this.bulkItems++
-    const priceOff = 0.50
-    if (this.bulkItems >= 3) {
-    }
+      const priceOff = 0.50
+    if (this.bulkItems >= 3) {}
     return bulkPrice
-	}
+  }
 
-	getNoRule(noRulePrice) {
+  getNoRule(noRulePrice) {
     return noRulePrice
   }
-  
+
   addTotal(prices) {
     if (prices.length > 0) {
       const reducer = (accumulator, currentValue) => accumulator + currentValue
